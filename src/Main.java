e75810528b4ce81dac8570fd4886d613ea1912f6
LoraFawaki <lora09.fawaki@gmail.com>
2024-07-12 21:16:22 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
+        System.out.println("Ye");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Batch 9 is great");
+            Thread.sleep(2000);
+        }
     }
 }
